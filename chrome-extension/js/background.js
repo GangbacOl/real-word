1843bf8f44f7fafe37a210b42b83b02a2d39b547
GangbacOl <gangyeonjo336@gmail.com>
2020-07-23 23:31:10 +0900

    [ADD] background.js 생성.

---
+++ chrome-extension/js/background.js
@@ -0,0 +1,17 @@
+var menuItem = {
+    id: 'addSelectionWord',
+    title: 'Add this word',
+    contexts: ['selection'],
+};
+
+chrome.contextMenus.create(menuItem);
+chrome.storage.sync.set({ words: [] });
+
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+    var selectionValue = clickData.selectionText;
+    chrome.storage.sync.get(['words'], function (result) {
+        var currentValue = result.words;
+        currentValue.push(selectionValue);
+        chrome.storage.sync.set({ words: currentValue });
+    });
+});
