cea5247606d6cf43c9a9ff8f774228bab843f0b5
GangbacOl <gangyeonjo336@gmail.com>
2020-07-25 23:40:40 +0900

    [UPD] chrome.storage 저장된 단어들 출력.

---
+++ chrome-extension/js/options.js
@@ -0,0 +1,7 @@
+chrome.storage.sync.get(['words'], function (result) {
+    for (var item in result.words) {
+        var li = document.createElement('li');
+        li.innerText = result.words[item];
+        document.querySelector('body > ol').append(li);
+    }
+});
