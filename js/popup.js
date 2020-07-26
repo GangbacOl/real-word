c36173741b0861f3404355416efe0a0b68bc1461
GangbacOl <gangyeonjo336@gmail.com>
2020-07-27 01:53:11 +0900

    [UPD] 저장한 단어의 유무 흐름제어.

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,9 @@
 chrome.storage.sync.get(['words'], function (result) {
+    if (!result.words[0]) {
+        var li = document.createElement('span');
+        li.innerText = '저장한 단어가 없음.';
+        document.querySelector('body > div.main > div').append(li);
+    }
     for (var i = result.words.length - 1; i > result.words.length - 4; i--) {
         if (!result.words[i]) break;
         var li = document.createElement('li');
