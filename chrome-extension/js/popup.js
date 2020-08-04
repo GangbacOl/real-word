e85e7e319dfade74f50f58a35de7295cc058387b
GangbacOl <gangyeonjo336@gmail.com>
2020-08-04 17:12:01 +0900

    [UPD] 데이터 구조 변경에 따른 코드 업데이트.

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -1,19 +1,13 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     document.querySelector('body > div.footer > input[type=button]').addEventListener('click', function () {
-//         chrome.tabs.create({ url: 'html/options.html' });
-//     });
-// });
-
-chrome.storage.sync.get(['words'], (storageData) => {
-    if (!storageData.words[0]) {
+chrome.storage.sync.get(['wordsList'], (storageData) => {
+    if (!storageData.wordsList[0].words[0]) {
         const li = document.createElement('span');
         li.innerText = '저장한 단어가 없음.';
         document.querySelector('body > div.main > div').append(li);
     }
-    for (let i = storageData.words.length - 1; i > storageData.words.length - 4; i--) {
-        if (!storageData.words[i]) break;
+    for (let i = storageData.wordsList[0].words.length - 1; i > storageData.wordsList[0].words.length - 4; i--) {
+        if (!storageData.wordsList[0].words[i]) break;
         const li = document.createElement('li');
-        li.innerText = storageData.words[i];
+        li.innerText = storageData.wordsList[0].words[i];
         document.querySelector('body > div.main > div').append(li);
     }
 });
