b09e924e984990d6d24eff1cfffe36c0d22d7670
GangbacOl <gangyeonjo336@gmail.com>
2020-10-15 23:32:00 +0900

    [UPD] checkDayPass 함수 작성 및 사용.

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -4,6 +4,14 @@ var menuItem = {
     contexts: ['selection'],
 };
 
+const checkDayPass = (day) => {
+    const today = new Date().format('dd');
+    // 하루가 지남
+    if (today !== day.date.slice(8, 10)) return true;
+    // 하루가 안 지남
+    else false;
+};
+
 chrome.contextMenus.create(menuItem);
 chrome.storage.sync.set({
     wordsList: [
@@ -18,6 +26,13 @@ chrome.contextMenus.onClicked.addListener(function (clickData) {
     var selectionValue = clickData.selectionText;
     console.log(selectionValue);
     chrome.storage.sync.get(['wordsList'], function (storageData) {
+        if (checkDayPass(storageData.wordsList[storageData.wordsList.length - 1])) {
+            console.log('good morning');
+            storageData.wordsList.unshift({
+                date: new Date().format('yyyy-MM-dd(KS)'),
+                words: [],
+            });
+        }
         console.log(storageData);
         var currentValue = storageData;
         currentValue.wordsList[0].words.push(selectionValue);
